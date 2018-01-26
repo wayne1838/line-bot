adb16bbc5ab1d1b4973a9c4cea575b35d3a5857b
wayne1838 <wayne1838@yahoo.com.tw>
2018-01-26 08:42:09 +0800

    加入權限

--- index.js
+++ index.js
@@ -4,19 +4,21 @@ let linebot = require('linebot'),
 const config = require('./package.json'),
     util = require('util');
 let bot = linebot({
-    channelSecret: '25ffb3104b69334901d25f703fb6d9ae',
-    channelAccessToken: 'ZeOAKeuHO4/CVhzLNv4C4bP30RSv78SFovLi/1bylh3r/rkyfZWF4I95Pn2AS9b5LwoaF61UIUaq0Cy/kovrhBTksR8TYUEBbcklwfAKng4xk/LPYZkxplKMYV0FTPdFG1tEv0eIdjb3tr2PNMX6ZQdB04t89/1O/w1cDnyilFU='
+    channelSecret: '',
+    channelAccessToken: ''
 });
+//設定有權限的使用者ID
+let agreeID = ["Uf4bd6364fa8f00a5d8b779d8173b5ab7",]
 const linebotParser = bot.parser(),
     app = express();
-app.post('/webhook', linebotParser);
+app.post('/line', linebotParser);
 // 在 localhost 走 8080 port
 let server = app.listen(process.env.PORT || 3000, function() {
     let port = server.address().port;
     console.log("My Line bot App running on port", port);
      setTimeout(function(){
-        var userId = "U1eb026dd7325f88ffd1b331b7b6975c2";
-        var sendMsg = 'FDF';
+        var userId = "Uf4bd6364fa8f00a5d8b779d8173b5ab7";
+        var sendMsg = '開啟';
         bot.push(userId,sendMsg);
          bot.push('wayne1838',sendMsg);
         console.log('send: '+sendMsg);
@@ -30,27 +32,27 @@ bot.on('message', function(event) {
     console.log(event.source.userId + ' :' + event.message.text);
     //replayMsg(event);
     analysisMsg(event);
-
-    setTimeout(function(){
-        var userId = event.source.userId;
-        var sendMsg = 'FDF';
-        bot.push(userId,sendMsg);
-        console.log('send: '+sendMsg);
-    },5000);
   }
 });
 
 function analysisMsg(event) {
     //解析訊息
+    //圖片出處 https://meteor.today/a/mHfzMQ
     var txt = event.message.text;
     var msg = '?';
     var imgs = ['https://imgur.com/GfozkL2.jpg',
         'https://cdn2.ettoday.net/images/2457/d2457713.jpg',
         'https://luna.komica.org/12/src/1516616711084.jpg',
         'https://luna.komica.org/12/thumb/1516646875952s.jpg',
+        'https://i.imgur.com/CmN8KXq.jpg',
+        'https://i.imgur.com/wF8gsYg.png',
+        'https://i.imgur.com/JSzsAQP.jpg',
+        'https://i.imgur.com/pFFZpC1.jpg',
         ];
-
     switch(txt) {
+    case '?':
+        imgs = ['https://i.imgur.com/CmN8KXq.jpg'];
+        return replayImg(event,imgs);
     case '1':
         return replayImg(event,imgs);
     case '2':
@@ -62,6 +64,9 @@ function analysisMsg(event) {
     case 'id':
         msg =  event.source.userId
         return replayMsg(event,msg);
+    case 'gg':
+        msg =  event.source.userId
+        return replayImg(event,imgs,true);
     default:
         //msg =  "ggggg"
         //return replayMsg(event,msg);
@@ -71,7 +76,11 @@ function analysisMsg(event) {
 }
 
 
-function replayMsg(event,msg){
+function replayMsg(event,msg,agree = false){
+    if (agree == true && agreeID.indexOf(event.source.userId) == -1)
+    {//agree == true 檢查權限 沒有權限則離開
+        return ;
+    }
     //回傳msg
     return event.reply(msg).then(function(data) {
       // success
@@ -82,7 +91,11 @@ function replayMsg(event,msg){
     });
 }
 
-function replayImg(event,imgs){
+function replayImg(event,imgs,agree = false){
+    if (agree == true && agreeID.indexOf(event.source.userId) == -1)
+    {//agree == true 檢查權限 沒有權限則離開
+        return ;
+    }
     //取得隨機數
     var num = Math.ceil(Math.random()*imgs.length)-1;
 
@@ -92,5 +105,5 @@ function replayImg(event,imgs){
     type: 'image',
     originalContentUrl: imgs[num],
     previewImageUrl: imgs[num]
-});
+    });
 }
