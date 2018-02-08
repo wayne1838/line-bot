bd0698992dc0c88d9627e041c087000914b1407f
wayne1838 <wayne1838@yahoo.com.tw>
2018-02-08 10:41:30 +0800

    bug fix

--- PM2.5.js
+++ PM2.5.js
@@ -4,7 +4,7 @@ var request = require("request");
 var cheerio = require("cheerio");
 var getJSON = require('get-json'); //FOR PM2.5
 
-var timer;
+var timer,timerRain;
 var pm2_5 =[];
 var rainData =[];
 
@@ -14,26 +14,27 @@ function _getJSON() {
   //http://opendata2.epa.gov.tw/AQX.json--這網址好像是假資料?
   getJSON('http://opendata.epa.gov.tw/ws/Data/ATM00625/?$format=json', function(error, response) {
     response.forEach(function(e, i) {
-       //console.log("PM");
+       console.log("PM25"+ e.Site);
       pm2_5[i] = [];
       pm2_5[i][0] = e.Site;
-      pm2_5[i][1] = e.PM25 * 1;
+      pm2_5[i][1] = e.PM25;
       pm2_5[i][2] = e.DataCreationDate ;
     });
   });
   //console.log("PM");
-  if (!pm2_5){//取得資料失敗 三十秒重試
-    timer = setInterval(_getJSON, 30000);
-  }
-  timer = setInterval(_getJSON, 1800000); //每半小時抓取一次新資料
+  // if (pm2_5 == ""){//取得資料失敗 三十秒重試
+  //   timer = setInterval(_getJSON, 30000);
+  // }
+  timer = setInterval(_getJSON, 30000); //每半小時抓取一次新資料
 }
 //下雨 氣象meteorology  http://opendata.epa.gov.tw/ws/Data/RainTenMin/?$orderby=PublishTime%20desc&$skip=0&$top=1000&format=json
 function _getRain() {
-  console.log("Rain");
-  clearTimeout(timer);
+ 
+  clearTimeout(timerRain);
   //http://opendata2.epa.gov.tw/AQX.json--這網址好像是假資料?
   getJSON('http://opendata.epa.gov.tw/ws/Data/RainTenMin/?$format=json', function(error, response) {
     response.forEach(function(e, i) {
+       console.log("Rain"+ e.SiteName);
       rainData[i] = [];
       rainData[i][0] = e.SiteName;
       rainData[i][1] = e.Rainfall10min;
@@ -41,10 +42,10 @@ function _getRain() {
       rainData[i][3] = e.Rainfall3hr ;
     });
   });
-  if (!rainData){//取得資料失敗 三十秒重試
-    timer = setInterval(_getRain, 30000);
-  }
-  timer = setInterval(_getRain, 1800000); //每半小時抓取一次新資料
+  // if (rainData == ""){//取得資料失敗 三十秒重試
+  //   timer = setInterval(_getRain, 30000);
+  // }
+  timerRain = setInterval(_getRain, 30000); //每半小時抓取一次新資料
 }
 
 
